c5ffd90401fdca5d641eef06e3d62fbe462ba4da
hilad <hilad@mint.security>
2025-06-24 16:50:37 +0300

    update version to error mode instead of debug

--- Formula/mint-client-installer.rb
+++ Formula/mint-client-installer.rb
@@ -2,7 +2,7 @@ class MintClientInstaller < Formula
   desc "Mint MCP Proxy Server installer"
   homepage "https://github.com/Mint-Security/mint-mcp-proxy-server-installer"
   url "https://github.com/Mint-Security/mint-mcp-proxy-server-installer/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "4d22099e85adc0f54deeac7cf10d9571d41526896d0d465487676c84bbbc92b9"
+  sha256 "cf46844164321d02ff5ce1833dab01cc230e95c7ebca1625a9f56ceebff16964"
   license "Proprietary"
 
   depends_on "python@3.12"
