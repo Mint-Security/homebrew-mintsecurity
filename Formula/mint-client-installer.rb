fe5211d5cce22286c3f9c72128f3fc2f690f26ee
hilad <hilad@mint.security>
2025-06-27 10:38:48 +0300

    updated version

--- Formula/mint-client-installer.rb
+++ Formula/mint-client-installer.rb
@@ -2,7 +2,7 @@ class MintClientInstaller < Formula
   desc "Mint MCP Proxy Server installer"
   homepage "https://github.com/Mint-Security/mint-mcp-proxy-server-installer"
   url "https://github.com/Mint-Security/mint-mcp-proxy-server-installer/archive/refs/tags/v2.1.0.tar.gz"
-  sha256 "326ba12d07b98ce662d9216fb192683f303e285b9401ca6f8dd5f0c9da2c188d"
+  sha256 "48efa6d82ca3076a5c65b30158599e930fb5ecf7ca3ab141ef7b52f60782742e"
   license "Proprietary"
 
   depends_on "python@3.12"
